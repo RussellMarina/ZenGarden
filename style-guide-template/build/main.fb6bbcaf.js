f3a08cba18ecffafd60977a7aede406db88f2dab
niki1980 <38739103+niki1980@users.noreply.github.com>
2020-02-06 22:22:20 -0700

    updates to style guide
    changed logo size

--- style-guide-template/build/main.fb6bbcaf.js
+++ style-guide-template/build/main.fb6bbcaf.js
@@ -276,7 +276,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60601" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56328" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
