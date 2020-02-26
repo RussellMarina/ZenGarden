739df5475e8396228bab2e42e377c2df0c3b8401
RussellMarina <marinairussell@gmail.com>
2020-02-25 20:35:05 -0700

    svgs

--- style-guide-template/build/main.fb6bbcaf.js
+++ style-guide-template/build/main.fb6bbcaf.js
@@ -276,7 +276,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59693" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57209" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
